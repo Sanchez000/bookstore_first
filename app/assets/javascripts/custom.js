a1498801947b2e5401f6b93ff7a98ba6a842fae5
Sanchez <artemzub000@gmail.com>
2019-02-08 11:02:26 +0000

    add some js for read more button

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -3,3 +3,11 @@ $('body').on('click', '.dropdown-toggle', function (event) {
     event.preventDefault(); 
 });
 
+$('body').on('click', '.line-height-2', function (event) {
+    $('.line-height-2').readmore({
+    collapsedHeight: 15,
+    moreLink: '<a href="#">Read more</a>',
+    lessLink: '<a href="#">Read less</a>'
+    });
+    event.preventDefault(); 
+});
